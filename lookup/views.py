5532bd63dfe2bdbf83667f62e0b4068ced6a14dc
Valentin Nizamski <vn_mail@abv.bg>
2023-03-13 17:05:20 +0200

    new commit

--- lookup/views.py
+++ lookup/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# https://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=89129&distance=5&API_KEY=B5FD4BAC-0E9B-4BB1-A32B-E027BED4EAB9
+
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=89129&distance=5&API_KEY=B5FD4BAC-0E9B-4BB1-A32B-E027BED4EAB9")
+    
+    try:
+        api = json.load(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api' : api})
 
 def about(request):
     return render(request, 'about.html', {})
